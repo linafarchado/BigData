5b065e9abf8066ccc04db75ea4fa33270a8eb5c1
Florian Tigoulet <flotig77@gmail.com>
2024-04-02 11:35:48 +0200

    feat[analyzer]: Fixed mistakes in subject and added small cleanup for data

--- bourse/analyzer/analyzer.py
+++ bourse/analyzer/analyzer.py
@@ -7,18 +7,28 @@ import timescaledb_model as tsdb
 db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
 #db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
 
+def clean_data(df):
+    df = df.drop_duplicates()
+    df = df.dropna()
+
+    return df
+
 def store_file(name, website):
-    if db.is_file_done(name):
+    if db.is_file_done(name + ".bz2"):
+        print(db.is_file_done(name + ".bz2"))
         return
     if website.lower() == "boursorama":
         try:
-            df = pd.read_pickle("bourse/data/boursorama/" + name)  # is this dir ok for you ?
+            df = pd.read_pickle("/home/bourse/data/boursorama/" + name + ".bz2")  # is this dir ok for you ?
         except:
             year = name.split()[1].split("-")[0]
-            df = pd.read_pickle("bourse/data/boursorama/" + year + "/" + name)
+            df = pd.read_pickle("/home/bourse/data/boursorama/" + year + "/" + name + ".bz2")
         # to be finished
+ 
+        df = clean_data(df)
 
+        print(df)
 
 if __name__ == '__main__':
-    # store_file("compA 2020-01-01 09:02:02.532411", "boursorama")
+    store_file("amsterdam 2020-12-31 17:51:02.225763", "boursorama")
     print("Done")
