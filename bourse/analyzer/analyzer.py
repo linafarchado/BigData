2e44ade0230fcd9f606a7d60488aba6fd01bcda0
Florian Tigoulet <flotig77@gmail.com>
2024-04-03 15:52:54 +0200

    feat: can load all files from a certain date, group by name of stock and to a first cleanup

--- bourse/analyzer/analyzer.py
+++ bourse/analyzer/analyzer.py
@@ -2,10 +2,44 @@ import pandas as pd
 import numpy as np
 import sklearn
 
+import os
+
 import timescaledb_model as tsdb
 
 db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
 #db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
+
+def load_specific_date(spec):
+    year = spec.split("-")[0]
+    folder_path = "/home/bourse/data/boursorama/" + year + "/"
+
+    file_paths = []
+
+    for filename in os.listdir(folder_path):
+    # Check if filename contains the desired date
+        if spec in filename:
+            # Construct the full path
+            file_path = os.path.join(folder_path, filename)
+            file_paths.append(file_path)
+
+    data_frames = []
+    for file_path in file_paths:
+    # Read the CSV file into a DataFrame
+        df = pd.read_pickle(file_path)
+        data_frames.append(df)
+
+    # Combine all DataFrames into a single DataFrame (optional)
+    df_combined = pd.concat(data_frames, ignore_index=True)
+    return df_combined
+
+def first_clean(df_combined):
+    grouped_df = df_combined.groupby("name")
+
+    for name, group_df in grouped_df:
+        
+        for index, row in group_df.iterrows():
+            if "(c)" in str(row["last"]):
+                df_combined.loc[index, "last"] = row["last"][:-3]
 
 def clean_data(df):
     df = df.drop_duplicates()
@@ -30,5 +64,8 @@ def store_file(name, website):
         print(df)
 
 if __name__ == '__main__':
-    store_file("amsterdam 2020-12-31 17:51:02.225763", "boursorama")
+    #store_file("amsterdam 2020-12-31 17:51:02.225763", "boursorama")
+    df = load_specific_date("2022-12-01")
+    df2 = first_clean(df)
+    print(df2)
     print("Done")
