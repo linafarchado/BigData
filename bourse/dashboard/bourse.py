a5a482a8fa596de0124e0195ad01d4632c0653c0
Florian Tigoulet <flotig77@gmail.com>
2024-04-28 13:44:50 +0200

    fix: cleaned code for clarity purpose

--- bourse/dashboard/bourse.py
+++ bourse/dashboard/bourse.py
@@ -28,11 +28,11 @@ app.layout = html.Div([
                 html.Label('Time Period:'),
                 dcc.DatePickerRange(
                     id='date-picker-range',
-                    min_date_allowed=date(2020, 1, 1),  # Set a minimum allowed date (optional)
-                    max_date_allowed=date.today(),  # Set a maximum allowed date (optional)
-                    initial_visible_month=date.today(),  # Set the initially visible month
-                    start_date=date(2020, 1, 1),  # Set initial start date (last 30 days)
-                    end_date=date.today(),  # Set initial end date (today)
+                    min_date_allowed=date(2020, 1, 1),
+                    max_date_allowed=date.today(),
+                    initial_visible_month=date.today(),
+                    start_date=date(2019, 1, 1),
+                    end_date=date.today(),
                 ),
 
                 html.Label('Scale Type:'),
@@ -72,19 +72,65 @@ app.layout = html.Div([
                 html.Div(id='query-result')
              ])
 
+def get_stocks(symbol):
+    query = f"SELECT date, value FROM stocks WHERE cid = (SELECT mid FROM companies WHERE symbol = '{symbol}')"
+    return pd.read_sql_query(query, engine)
+
+def get_daystocks(symbol):
+    query = f"SELECT date, open, high, low, close FROM daystocks WHERE cid = (SELECT mid FROM companies WHERE symbol = '{symbol}')"
+    return pd.read_sql_query(query, engine)
+
+def update_shown_dates(stocks_df, start_date, end_date):
+    if start_date and end_date:
+        stocks_df = stocks_df[stocks_df['date'] >= start_date]
+        stocks_df = stocks_df[stocks_df['date'] <= end_date]
+    return stocks_df
+
+def display_raw_data(symbol, company_to_display, stocks_df, table_data):
+    if company_to_display == symbol:
+        daily_stats = stocks_df.resample('D', on='date').agg({
+            'value': ['min', 'max', 'mean', 'std'],
+            'date': ['first', 'last']
+        }).reset_index()
+
+        daily_stats.columns = [symbol, 'Min', 'Max', 'Mean', 'Std', 'First', 'Last']
+
+        daily_stats['Mean'] = daily_stats['Mean'].apply(lambda x: round(x, 5))
+        daily_stats['Std'] = daily_stats['Std'].apply(lambda x: round(x, 5))
+
+        table_data.append(html.Table(
+        children=[
+            html.Tr([html.Th(col) for col in daily_stats.columns])
+        ] + [
+            html.Tr([html.Td(val) for val in row]) for _, row in daily_stats.iterrows()
+        ]
+        ))
+
+    return table_data
+
+def get_candlestick_figure(daystocks_df):
+     return go.Figure(go.Candlestick(
+        x=daystocks_df['date'],
+        open=daystocks_df['open'],
+        high=daystocks_df['high'],
+        low=daystocks_df['low'],
+        close=daystocks_df['close']
+    ))
+
+
 @app.callback(
-    ddep.Output('company-dropdown', 'options'),  # Update output component
-    ddep.Input('update-companies', 'n_clicks')  # Update trigger
+    ddep.Output('company-dropdown', 'options'),
+    ddep.Input('update-companies', 'n_clicks')
 )
 def update_dropdown_options(n_clicks):
     if n_clicks > 0:
         try:
-            # Assuming your initial query retrieves company names
             company_df = pd.read_sql_query('SELECT name, symbol FROM companies', engine)
             return [{'label': row['name'] + " - " + row['symbol'], 'value': row['symbol']} for _, row in company_df.iterrows()]
+        
         except Exception as e:
-            return []  # Return an empty list in case of errors
-    return []  # Return an empty list initially
+            return []
+    return []
 
 @app.callback(
     [
@@ -103,65 +149,31 @@ def update_dropdown_options(n_clicks):
 )
 def update_stock_prices_graph(selected_companies, yaxis_type, start_date, end_date, graph_type, company_to_display):
     if selected_companies:
-        # try:
-            # Assuming your "stocks" table has "date" and "value" columns
+        try:
             stock_data, table_data = [], []
             dropdown_options = [{'label': company, 'value': company} for company in selected_companies]
 
-            for company in selected_companies:
-                query = f"SELECT date, value FROM stocks WHERE cid = (SELECT mid FROM companies WHERE symbol = '{company}')"
-                company_df = pd.read_sql_query(query, engine)
+            for symbol in selected_companies:
+                stocks_df = get_stocks(symbol)
+                daystocks_df = get_daystocks(symbol)
 
-                query = f"SELECT date, open, high, low, close FROM daystocks WHERE cid = (SELECT mid FROM companies WHERE symbol = '{company}')"
-                daystocks_df = pd.read_sql_query(query, engine)
-
-                if not company_df.empty:  # Check if data exists for the company
-                    company_df = company_df.sort_values(by='date')  # Sort by date
+                if not stocks_df.empty:
+                    stocks_df = stocks_df.sort_values(by='date')
                     daystocks_df = daystocks_df.sort_values(by='date')
 
-                    if start_date and end_date:
-                        company_df = company_df[company_df['date'] >= start_date]
-                        company_df = company_df[company_df['date'] <= end_date]
+                    stocks_df = update_shown_dates(stocks_df, start_date, end_date)
 
-                    if company_to_display == company:
-                        # Calculate daily min, max, mean, std, first, and last values
-                        daily_stats = company_df.resample('D', on='date').agg({
-                            'value': ['min', 'max', 'mean', 'std'],
-                            'date': ['first', 'last']
-                        }).reset_index()
-
-                        daily_stats.columns = [company, 'Min', 'Max', 'Mean', 'Std', 'First', 'Last']
-
-                        # Round mean and std columns to 5th decimal place
-                        daily_stats['Mean'] = daily_stats['Mean'].apply(lambda x: round(x, 5))
-                        daily_stats['Std'] = daily_stats['Std'].apply(lambda x: round(x, 5))
-
-                        table_data.append(html.Table(
-                        # Define table headers and rows
-                        children=[
-                            html.Tr([html.Th(col) for col in daily_stats.columns])
-                        ] + [
-                            html.Tr([html.Td(val) for val in row]) for _, row in daily_stats.iterrows()
-                        ]
-                        ))
+                    table_data = display_raw_data(symbol, company_to_display, stocks_df, table_data)
 
                     if graph_type == 'Candlestick':
-                        fig = go.Figure(go.Candlestick(
-                            x=daystocks_df['date'],
-                            open=daystocks_df['open'],
-                            high=daystocks_df['high'],
-                            low=daystocks_df['low'],
-                            close=daystocks_df['close']
-                        ))
-
+                        fig = get_candlestick_figure(daystocks_df)
                         return fig, table_data, dropdown_options
-                        #stock_data.append(fig)  # Append the entire figure for the company
-                       
+                        #stock_data.append(fig)
                     else:
                         stock_data.append({
-                            'x': company_df['date'],
-                            'y': company_df['value'],
-                            'name': company
+                            'x': stocks_df['date'],
+                            'y': stocks_df['value'],
+                            'name': symbol
                         })
 
             figure = {
@@ -171,13 +183,10 @@ def update_stock_prices_graph(selected_companies, yaxis_type, start_date, end_da
                 }
             }
 
-            # Set xaxis type based on radio button selection
-            #figure.update_yaxes(type='linear' if yaxis_type == 'Linear' else 'log')
             return figure, table_data, dropdown_options
-        # except Exception as e:
-        #     print(f"An error occurred: {e}")  # Print the exception details
-        #     return (), []
-    return {}, [], {}  # Return empty figure initially
+        except Exception as e:
+            return {}, [], {}
+    return {}, [], {}
 
 @app.callback( ddep.Output('query-result', 'children'),
                ddep.Input('execute-query', 'n_clicks'),
@@ -189,6 +198,7 @@ def run_query(n_clicks, query):
         try:
             result_df = pd.read_sql_query(query, engine)
             return html.Pre(result_df.to_string())
+        
         except Exception as e:
             return html.Pre(str(e))
     return "Enter a query and press execute."
