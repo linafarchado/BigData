ee3661b28d450499366b43ba7f5ea453b0ce2645
Florian Tigoulet <flotig77@gmail.com>
2024-04-30 00:30:05 +0200

    feat: Bollinger bands and cleaned a bit the dashboard

--- bourse/dashboard/bourse.py
+++ bourse/dashboard/bourse.py
@@ -19,6 +19,16 @@ engine = sqlalchemy.create_engine(DATABASE_URI)
 app = dash.Dash(__name__,  title="Bourse", suppress_callback_exceptions=True) # , external_stylesheets=external_stylesheets)
 server = app.server
 logging.basicConfig(level=logging.DEBUG)
+
+frequency_options = {
+    '10 Minutes': '10min',
+    'Hourly': 'h',
+    'Daily': 'D',
+    'Weekly': 'W',
+    'Monthly': 'ME',
+    'Yearly': 'YE'
+}
+
 app.layout = html.Div([
 
                 html.Label('Multi-Select Company Dropdown'),
@@ -50,6 +60,24 @@ app.layout = html.Div([
                     id='graph-type',
                     labelStyle={'display': 'inline-block', 'marginTop': '5px'}
                 ),
+
+                html.Label('Show Bollinger Bands:'),
+                dcc.Checklist(['Bollinger Bands'], [],
+                    id='show-bollinger-bands'
+                ),
+                html.Label('Bollinger Bands Window:'),
+                dcc.Input(
+                    id='bollinger-window',
+                    type='number',
+                    value=20
+                ),
+                html.Label('Data Frenquency:'),
+                dcc.Dropdown(
+                id='resample-frequency',
+                options=[{'label': k, 'value': v} for k, v in frequency_options.items()],
+                value='10min'
+            ),
+            html.Div(id='output-container', children=[]),
 
                 dcc.Graph(id='stock-prices-graph'),
 
@@ -86,6 +114,14 @@ def update_shown_dates(stocks_df, start_date, end_date):
         stocks_df = stocks_df[stocks_df['date'] <= end_date]
     return stocks_df
 
+def update_frequence_data(stocks_df, frequency):
+    daily_stats = stocks_df.resample(frequency, on='date').agg({
+            'value': ['last']
+        }).reset_index()
+    daily_stats.columns = ['date', 'value']
+
+    return daily_stats.dropna()
+
 def display_raw_data(symbol, company_to_display, stocks_df, table_data):
     if company_to_display == symbol:
         daily_stats = stocks_df.resample('D', on='date').agg({
@@ -108,15 +144,67 @@ def display_raw_data(symbol, company_to_display, stocks_df, table_data):
 
     return table_data
 
-def get_candlestick_figure(daystocks_df):
-     return go.Figure(go.Candlestick(
-        x=daystocks_df['date'],
-        open=daystocks_df['open'],
-        high=daystocks_df['high'],
-        low=daystocks_df['low'],
-        close=daystocks_df['close']
-    ))
+def calculate_bollinger_bands(stocks_df, window=20):
+    stocks_df['mean'] = stocks_df['close'].rolling(window).mean()
+    stocks_df['std'] = stocks_df['close'].rolling(window).std()
+    upper_band = stocks_df['mean'] + (2 * stocks_df['std'])
+    lower_band = stocks_df['mean'] - (2 * stocks_df['std'])
 
+    upper_band, lower_band = upper_band.to_frame(name='Upper Bollinger Band'), lower_band.to_frame(name='Lower Bollinger Band')
+
+    upper_band_trace = {
+        'x': stocks_df['date'],
+        'y': upper_band['Upper Bollinger Band'],
+        'name': 'Upper Band',
+        'fill' : 'topreviousy',
+        'fillcolor': 'rgba(217, 217, 217, 0.5)',
+        'showlegend': False,
+        'line': {'color': 'rgba(0, 0, 102, 0.3)'}
+    }
+    lower_band_trace = {
+        'x': stocks_df['date'],
+        'y': lower_band['Lower Bollinger Band'],
+        'fill' : 'tonexty',
+        'fillcolor': 'rgba(217, 217, 217, 0.5)',
+        'name': 'Lower Band',
+        'showlegend': False,
+        'line': {'color': 'rgba(128, 0, 0, 0.3)'}
+    }
+    average_trace = {
+        'x': stocks_df['date'],
+        'y': stocks_df['mean'],
+        'name': 'Simple Moving Average',
+        'showlegend': False,
+        'line': {'color': 'rgba(0, 0, 0, 0.3)'}
+    }
+    
+    return upper_band_trace, lower_band_trace, average_trace
+
+def create_line_data(frequency_df, graph_type, frequency, symbol):
+    daily_stats = frequency_df.resample(frequency, on='date').agg({
+            'value': ['first', 'max', 'min', 'last']
+        }).reset_index()
+    daily_stats.columns = ['date', 'open', 'high', 'low', 'close']
+    daily_stats.dropna(inplace=True)
+    
+    if graph_type == 'Candlestick':
+        line_data = {
+            'x': daily_stats['date'],
+            'open': daily_stats['open'],
+            'high': daily_stats['high'],
+            'low': daily_stats['low'],
+            'close': daily_stats['close'],
+            'type': 'candlestick',
+            'name': symbol
+        }
+    else:
+        line_data = {
+            'x': daily_stats['date'],
+            'y': daily_stats['close'],
+            'name': symbol
+        }
+
+    return line_data, daily_stats
 
 @app.callback(
     ddep.Output('company-dropdown', 'options'),
@@ -144,12 +232,15 @@ def update_dropdown_options(n_clicks):
         ddep.Input('date-picker-range', 'start_date'),
         ddep.Input('date-picker-range', 'end_date'),
         ddep.Input('graph-type', 'value'),
-        ddep.Input('company-to-display', 'value')
+        ddep.Input('company-to-display', 'value'),
+        ddep.Input('show-bollinger-bands', 'value'),
+        ddep.Input('bollinger-window', 'value'),
+        ddep.Input('resample-frequency', 'value')
     ]
 )
-def update_stock_prices_graph(selected_companies, yaxis_type, start_date, end_date, graph_type, company_to_display):
+def update_stock_prices_graph(selected_companies, yaxis_type, start_date, end_date, graph_type, company_to_display, show_bollinger_bands, bollinger_window, frequency):
     if selected_companies:
-        try:
+        # try:
             stock_data, table_data = [], []
             dropdown_options = [{'label': company, 'value': company} for company in selected_companies]
 
@@ -165,27 +256,26 @@ def update_stock_prices_graph(selected_companies, yaxis_type, start_date, end_da
 
                     table_data = display_raw_data(symbol, company_to_display, stocks_df, table_data)
 
-                    if graph_type == 'Candlestick':
-                        fig = get_candlestick_figure(daystocks_df)
-                        return fig, table_data, dropdown_options
-                        #stock_data.append(fig)
+                    line_data, frequency_df = create_line_data(stocks_df.copy(), graph_type, frequency, symbol)
+
+                    if 'Bollinger Bands' in show_bollinger_bands:
+                        upper_band, lower_band, sma_line = calculate_bollinger_bands(frequency_df.copy(), bollinger_window)
+
+                        stock_data.extend([line_data, upper_band, lower_band, sma_line])
                     else:
-                        stock_data.append({
-                            'x': stocks_df['date'],
-                            'y': stocks_df['value'],
-                            'name': symbol
-                        })
+                        stock_data.append(line_data)
 
             figure = {
                 'data': stock_data,
                 'layout': {
-                    'yaxis': {'type': 'linear' if yaxis_type == 'Linear' else 'log'}  # Set y-axis type in layout
+                    'yaxis': {'type': 'linear' if yaxis_type == 'Linear' else 'log'},
+                    'xaxis': {'rangeslider': {'visible': False}}
                 }
             }
 
             return figure, table_data, dropdown_options
-        except Exception as e:
-            return {}, [], {}
+        # except Exception as e:
+        #     return {}, [], {}
     return {}, [], {}
 
 @app.callback( ddep.Output('query-result', 'children'),
