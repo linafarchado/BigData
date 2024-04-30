1ab97a69df626eccc627427115f7ce6e84741605
Lina Farchado <lina.farchado@epita.fr>
2024-04-30 11:59:26 +0200

    feat(load): save

--- bourse/analyzer/analyzer.py
+++ bourse/analyzer/analyzer.py
@@ -1,15 +1,16 @@
 import pandas as pd
+import numpy as np
 import os
+import dask.dataframe as dd
 import timescaledb_model as tsdb
-from multiprocessing import Pool, cpu_count
 import logging
-from functools import partial
-
-# Configurez la journalisation
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler('process.log'), logging.StreamHandler()])
+from multiprocessing import Pool, cpu_count
+from tqdm import tqdm
 
 db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
 #db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
+
+logging.basicConfig(level=logging.DEBUG)
 
 def clean_c_s(df):
     grouped_df = df.groupby("name")
@@ -22,44 +23,22 @@ def clean_c_s(df):
     return df
 
 def clean_data(df):
-    logging.debug("Cleaning the data")
     df1 = df.drop_duplicates()
     df2 = df1.dropna()
 
     return clean_c_s(df2)
 
-def preprocess_boursorama_data(df):
-    # Groupe les données par date et symbole
-    grouped_data = df.groupby(['date', 'symbol'])
-
-    # Initialise une liste pour stocker les DataFrames traités
-    processed_dfs = []
-
-    # Itère sur les groupes
-    for group_name, group_df in grouped_data:
-        date, symbol = group_name
-
-        # Sélectionne les valeurs uniques pour les colonnes open, high, low, close, volume
-        group_df = group_df[['open', 'high', 'low', 'close', 'volume']].agg(['max', 'min'])
-        group_df.columns = ['_'.join(col).strip('_') for col in group_df.columns.values]
-        group_df = group_df.reset_index(drop=True)
-        group_df['date'] = date
-        group_df['symbol'] = symbol
-
-        processed_dfs.append(group_df)
-
-    # Concatène tous les DataFrames traités en un seul DataFrame
-    processed_df = pd.concat(processed_dfs, ignore_index=True)
-
-    return processed_df
-
 # Add the data to the companies table
 def add_companies(df):
-    logging.debug("Adding data to the companies table")
+    logging.debug(f'In add_companies')
+
+    # Remove all duplicates in the name
+    unique_names_df = df.drop_duplicates(subset=['symbol']).reset_index()
+
     comp_df = pd.DataFrame({
-        "name": df["name"].copy(),
-        "mid": df.index,
-        "symbol": df["symbol"].copy(),
+        "name": unique_names_df["name"].copy(),
+        "mid": unique_names_df.index,
+        "symbol": unique_names_df["symbol"].copy(),
         "symbol_nf": None,
         "isin": None,
         "reuters": None,
@@ -68,13 +47,15 @@ def add_companies(df):
         "sector": None
     })
 
+    db.execute("ALTER SEQUENCE company_id_seq RESTART WITH 1", commit=True)
     db.df_write(comp_df, "companies", index=False, if_exists="replace", commit=True)
 
     return comp_df
 
 # Add the data to the stocks table
 def add_stocks(df, comp_dict):
-    logging.debug("Adding data to the stocks table")
+    logging.debug(f'In add_stocks')
+
     df['id'] = df['symbol'].apply(lambda x: comp_dict.get(x))
 
     stocks_df = pd.DataFrame({
@@ -86,9 +67,12 @@ def add_stocks(df, comp_dict):
 
     db.df_write(stocks_df, "stocks", index=False, if_exists="replace", commit=True)
 
+    return stocks_df
+
 # Add the data to the daystocks table
 def add_daystocks(df, comp_dict):
-    logging.debug("Adding data to the daystocks table")
+    logging.debug(f'In add_daystocks')
+
     df['id'] = df['symbol'].apply(lambda x: comp_dict.get(x))
 
     daystocks_df = pd.DataFrame({
@@ -103,65 +87,93 @@ def add_daystocks(df, comp_dict):
 
     db.df_write(daystocks_df, "daystocks", index=False, if_exists="replace", commit=True)
 
+    return daystocks_df
+
 # Add the data to the file_done table
-def add_file_done(file_name):
-    logging.debug(f"Adding {file_name} to the file_done table")
+def add_file_done(df):
+    logging.debug(f'In add_file_done')
+
     filedone_df = pd.DataFrame({
-        "name": [file_name]
+        "name": df["filename"].unique().copy()
     })
 
     db.df_write(filedone_df, "file_done", index=False, if_exists="replace", commit=True)
+
+    return filedone_df
 
 def make_companies_dict(df):
     comp_dict = df.set_index('symbol')['mid'].to_dict()
 
     return comp_dict
 
-# Process a single file and add it to the database
-def process_file(file_path):
-    try:
-        # If the file was already added to the Database, skip it
-        if db.is_file_done(file_path):
-            logging.debug(f"The file {file_path} has already been added before")
-            return
-        
-        # Read and clean the file
-        df = pd.read_pickle(file_path)
-        df['date'] = file_path.split(" ")[1] + " " + file_path.split(" ")[2].split(".")[0] # We add the date
-        df['filename'] = file_path # We add the file_name
+# Er add everyting to the database
+def add_to_database(df):
+    logging.debug(f'In add_to_database')
 
-        df_final = clean_data(df)
-        df_processed = df_final # preprocess_boursorama_data(df_final)
+    comp_df = add_companies(df)
 
-        # Add the data to the database
-        comp_df = add_companies(df_processed)
-        comp_dict = make_companies_dict(comp_df)
-        add_daystocks(df_processed, comp_dict)
-        add_stocks(df_processed, comp_dict)
-        add_file_done(file_path)
+    comp_dict = make_companies_dict(comp_df)
 
-        logging.debug(f"File {file_path} processed successfully")
-    except Exception as e:
-        logging.error(f"Error processing file {file_path}: {e}")
+    add_daystocks(df.copy(), comp_dict)
+    add_stocks(df.copy(), comp_dict)
+    add_file_done(df)
 
-def fill_database():
+def store_file(name):
+    #logging.debug(f'In store_file')
+
+
+    # If the file was already added to the Database, skip it
+    """ if db.is_file_done(name):
+        print(f"The file {name} has already been added before")
+        return None"""
     
+    # Else we add read and clean it
+    df = pd.read_pickle(name)
+    df['date'] = name.split(" ")[1] + " " + name.split(" ")[2].split(".")[0] # We add the date
+    df['filename'] = name # We add the file_name
+
+    df_final = clean_data(df)
+
+    return df_final
+    
+def run_imap_unordered_multiprocessing(func, argument_list, num_processes):
+
+    pool = Pool(processes=num_processes)
+
+    cpt = 0
+
+    result_list_tqdm = []
+    for result in tqdm(pool.imap_unordered(func=func, iterable=argument_list), total=len(argument_list)):
+        cpt += 1
+        logging.debug(f"Processed file {cpt}")
+        result_list_tqdm.append(result)
+
+    return result_list_tqdm
+
+def load_all_files():
+    logging.debug(f'In load_all_files')
+
     folder_path = "/home/bourse/data/boursorama/"
     file_paths = []
     for year_folder in os.listdir(folder_path):
         year_path = os.path.join(folder_path, year_folder)
         if os.path.isdir(year_path):
-            file_paths.extend([os.path.join(year_path, file_name) for file_name in os.listdir(year_path)])
+            file_paths.extend([os.path.join(year_path, file_name) for file_name in os.listdir(year_path) if not db.is_file_done(file_name)])
 
     logging.debug(f"Total number of files to process: {len(file_paths)}")
-    # Parallélisation avec multiprocessing
-    num_processes = cpu_count() * 2
-    with Pool(processes=num_processes) as pool:
-        pool.map(process_file, file_paths)
-        pool.close()
-        pool.join()
 
-    logging.debug("Done")
+    dfs = []
+    num_processes = cpu_count() * 2
+    return run_imap_unordered_multiprocessing(store_file, file_paths, num_processes)
+
+def fill_database():
+    logging.debug("In fill_database")
+
+    df = load_all_files()
+    add_to_database(df)
 
 if __name__ == '__main__':
+    logging.debug(f'In MAIN')
+
     fill_database()
+    print("Done")
